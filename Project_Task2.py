9218a5ba7b10cde26a3ac58d134f7fe42cef32af
Rida_Q <ridaqasim50@gmail.com>
2022-04-05 16:36:37 +0300

    Task2-Draft3 (Partial results- removed duplicate data records)

--- Project_Task2.py
+++ Project_Task2.py
@@ -5,8 +5,7 @@ Created on Sat Apr  2 17:48:12 2022
 @author: Rida
 """
 
-import re
-from multiprocessing import Pool
+
 import multiprocessing as mp
 from collections import Counter
 
@@ -16,7 +15,7 @@ def mapper(x):
     return(cols[0], cols[1])
 
 
-
+"""
 def reducer(y):
     
     count = 0
@@ -31,17 +30,46 @@ def reducer(y):
                 max_count = count
     
     return max_count
+"""
+def combiner(y):
+    
+    res = list(Counter(key for key, num in y
+                       for idx in range(num)).items())
+    return res
+    
+    
+
+def reducer(z):
+    
+    passenger, flights = z
+    count = len(flights)
+    
+
+    return (passenger, count)
+
+"""
+def reducer2(m):
+    max = 0
+    i,j = m
+    if(m > max):
+        
+        max = j;
+
+    return (i, max)
+"""   
+  
+
 
 def shuffler(map_out):
     data = {}
-    map_out = list(filter(None, map_out))
+    map_out = set(list(filter(None, map_out)))
     for k, v in map_out:
         if k not in data:
             data[k] = [v]
         else:
             data[k].append(v)
             
-    return data
+    return (data)
     
 mapper_in = []
 
@@ -52,5 +80,5 @@ if __name__ == "__main__":
     with mp.Pool(processes=mp.cpu_count()) as pool:
         mapper_out = pool.map(mapper, mapper_in, chunksize=int(len(mapper_in)/mp.cpu_count()))
         reducer_in = shuffler(mapper_out)
-        reducer_out = pool.map(reducer, reducer_in.items(), chunksize=int(len(reducer_in.keys())/mp.cpu_count()))
-        print(reducer_out)
+        #reducer_out = pool.map(reducer, reducer_in.items(), chunksize=int(len(reducer_in.keys())/mp.cpu_count()))
+        print(reducer_in)
