d1a9f7e2e842c309afeca48684f6703be2424646
Rida_Q <ridaqasim50@gmail.com>
2022-04-25 04:26:10 +0100

    Comments and Optimization

--- Project_Task1.py
+++ Project_Task1.py
@@ -1,73 +1,68 @@
 
+# Importing essential libraries
 import multiprocessing as mp
+import pandas as pd
+
+##########################################################
 
 """ INITIALIZING FUNCTIONS FOR MULTI-PROCESSING """
 
 ###########################################################
 
-def mapper1(m):
+""" MAPPER FUNCTION FOR FIRST MAP JOB (i.e. Mapping Airport names with FAA code) """
+
+def mapper1(m1):
     
-    cols = m.split(',')
+    # Comma delimiter template 
+    cols = m1.split(',')
     
     return (cols[0], cols[1])
 
 ############################################################
+
+""" MAPPER FUNCTION FOR SECOND MAP JOB (i.e. Mapping FAA code of departure airport with Flight id) """
     
-def mapper2(n):
-    cols = n.split(',')
+def mapper2(m2):
+    
+    # Comma delimiter template 
+    cols = m2.split(',')
     
     return (cols[2], cols[1])
 
 #############################################################
 
-def reducer(y):
-    airport, flights = y
+""" REDUCER FUNCTION """
+
+def reducer(z):
+    airport, flights = z
+    
+    # Counting number of flights for each airport
     count = len(flights)
     
     return(airport, count)
 
+
 ##############################################################
 
-"""
-def shuffler(map_out1, map_out2):
-    data = {}
-    map_out1 = list(filter(None, map_out1))
-    map_out2 = list(filter(None, map_out2))
-    
+""" COMBINER FUNCTION (To aggregarte/combine results of both mappers) """
 
-    for i,j in map_out1:
-        
-        if i not in data:
-            data[i] = [j]
-        else:
-            data[i].append(j)
+def combiner(x, y):
 
-      
-        for k,v in map_out2:
-            
-            if (k == data.items()):
-                data[k] = j[v]
-                data[i] = data[k]
-            else:
-                 data[k].append(v)
-            
-    return data
- """      
-##############################################################
-
-def shuffler(airport_map, flight_map):
     codes = {}
     flights = {}
-    airport_map = set(list(filter(None, airport_map)))
-    flight_map = set(list(filter(None, flight_map)))
     
+    # Removing duplicates in file
+    airport_map = set(list(filter(None, x)))
+    flight_map = set(list(filter(None, y)))
     
+    # Iterating through first list
     for i,j in airport_map:
         if j not in codes:
             codes[j] = [i]
         else:
             codes[j].append(i)
-        
+            
+    # Iterating on second list and grouping results by FAA codes   
     for k,v in flight_map:
         if codes[k][0] not in flights:
             flights[codes[k][0]]= [v]
@@ -75,25 +70,60 @@ def shuffler(airport_map, flight_map):
             flights[codes[k][0]].append(v)
             
     return flights
+
+#################################################################
+
+""" INPUT FILE FORMAT """
+
+def readInputFile(a):
+    
+    map_in=[]
+    
+    with open(a, encoding='utf8') as f:
+        
+        # Line split to iterate through each row 
+        map_in = f.read().splitlines()
+        
+        # Removing Null values from input records
+        map_in= list(filter(None, map_in))
+        
+    return (map_in)
              
 #################################################################
 
+""" MAIN FUNCTION """
+
+def main():
+     map_in1 = readInputFile('C:\\Users\\omer_\\Downloads\\cc_work\\Top30_airports_LatLong(1).csv')
+     map_in2 = readInputFile('C:\\Users\\omer_\\Downloads\\cc_work\\Passenger_data.csv')
+        
+    # Multi-processing (Thread corresponding functions)    
+     with mp.Pool(processes=mp.cpu_count()) as pool:
+        
+        # Mapper Jobs
+        map_out1 = pool.map(mapper1, map_in1, chunksize=int(len(map_in1)/mp.cpu_count()))
+        map_out2 = pool.map(mapper2, map_in2, chunksize=int(len(map_in2)/mp.cpu_count()))
+        
+        # Combiner Job
+        reducer_in = combiner(map_out1, map_out2)
+        
+        # Reducer Job
+        reducer_out = pool.map(reducer, reducer_in.items(), chunksize=int(len(reducer_in.keys())/mp.cpu_count()))
+        
+        # Displaying Output in Table format
+        headings = ["Airport Name", "No. of Flights"]
+        Table = pd.DataFrame(columns = headings, data = reducer_out)
+        print('"Total flights from each airport"\n\n',Table)
+        
+        # Output to csv file
+        Table.to_csv('Task1_Results.csv', index = False, header = True)
+  
+#################################################################
+
 """ ENTERING MAIN FILE FOR EXECUTION """
- 
-mapper_in = []
 
 if __name__ == "__main__":
-    with open('C:\\Users\\omer_\\Downloads\\cc_work\\Top30_airports_LatLong(1).csv', encoding='utf8') as f1:
-         a = f1.read().splitlines()
-         mapper_in1= list(filter(None, a))
-         
-    with open('C:\\Users\\omer_\\Downloads\\cc_work\\Passenger_data.csv', encoding='utf8') as f2:
-        b = f2.read().splitlines()
-        mapper_in2= list(filter(None, b))
-        
-    with mp.Pool(processes=mp.cpu_count()) as pool:
-        mapper_out1 = pool.map(mapper1, mapper_in1, chunksize=int(len(mapper_in1)/mp.cpu_count()))
-        mapper_out2 = pool.map(mapper2, mapper_in2, chunksize=int(len(mapper_in2)/mp.cpu_count()))
-        reducer_in = shuffler(mapper_out1, mapper_out2)
-        reducer_out = pool.map(reducer, reducer_in.items(), chunksize=int(len(reducer_in.keys())/mp.cpu_count()))
-        print(reducer_out)
+    
+    main()
+     
+#################################################################
--- Project_Task2.py
+++ Project_Task2.py
@@ -1,49 +1,60 @@
 
-
+# Importing essential libraries
 import multiprocessing as mp
+import pandas as pd
+
+#########################################################
 
 """ DEFINING FUNCTIONS FOR MULTI-PROCESSING """
 
 #########################################################
 
+""" MAPPER FUNCTION"""
+
 def mapper(x):
+    
+    # Comma delimiter template
     cols = x.split(',')
-    #if re.match('^\\d{1,9}$', cols[1]):
+        
     return(cols[0], cols[1])
 
 
 #########################################################
+
+""" REDUCER FUNCTION """
+
+def reducer(z):
     
+    # Calculating passenger/passengers having maximum number of flights
+    passengers = [(key, value) for key,value in z.items() if value == max(z.values())]
+    
+    return (passengers)
+
+#########################################################
+
+""" COMBINER FUNCTION """
+
 def combiner(y):
     
     passenger, flights = y
+    
+    # Counting number of flights for each passenger
     count = len(flights)
     
     return (passenger,count)
 
-#########################################################
-
-
-def reducer(z):
-    
-    max_val = 0
-    passengers = []
-    for passenger in z.keys():
-        if z[passenger]> max_val:
-            max_val =  z[passenger]
-            passengers = [passenger]
-        elif z[passenger] == max_val:
-            passengers.append(passenger)
-            
-    
-    return (passengers, max_val)
-
 
 #######################################################
 
+""" SORTER FOR MAPPER OUTPUT """
+
 def sorter1(map_out):
     data = {}
+    
+    # Filtering duplicates from mapper list
     map_out = set(list(filter(None, map_out)))
+    
+    # Sorting mapper results for Combiner function
     for k, v in map_out:
         if k not in data:
             data[k] = [v]
@@ -54,28 +65,77 @@ def sorter1(map_out):
 
 #########################################################
 
+""" SORTER FOR COMBINER OUTPUT """
 
 def sorter2(comb_out):
-    data1 = {}
+    
+    data = {}
+    
+    # Filtering duplicates from combiner list
     comb_out = list(filter(None, comb_out))
+    
+    
+    # Transforming list of tuples to Dictionary of (key:value) pairs for reducer job
     for k, v in comb_out:
-       data1[k] = v
+       data[k] = v
             
-    return (data1)
+    return (data)
 
 #########################################################
 
-""" ENTERING MAIN FILE FOR EXECUTION """
-    
-mapper_in = []
+""" INPUT FILE FORMAT """
 
-if __name__ == "__main__":
-    with open('C:\\Users\\omer_\\Downloads\\cc_work\\Passenger_data.csv', encoding='utf8') as f:
-        mapper_in = f.read().splitlines()
+def readInputFile(a):
+    
+    map_in=[]
+    
+    with open(a, encoding='utf8') as f:
         
+        # Line split to iterate through each row 
+        map_in = f.read().splitlines()
+        
+        # Removing Null values from input records
+        map_in= list(filter(None, map_in))
+        
+    return (map_in)
+
+#########################################################
+
+""" MAIN FUNCTION """
+
+def main():
+    
+    # Add input file as mapper input
+    mapper_in = readInputFile('C:\\Users\\omer_\\Downloads\\cc_work\\Passenger_data.csv')  
+    
+    # Pooling function to carry our multi-processing
     with mp.Pool(processes=mp.cpu_count()) as pool:
+        
+        # Mapper job
         mapper_out = pool.map(mapper, mapper_in, chunksize=int(len(mapper_in)/mp.cpu_count()))
+        
+        # Combiner Job
         combiner_in = sorter1(mapper_out)
         combiner_out = pool.map(combiner, combiner_in.items(), chunksize=int(len(combiner_in.keys())/mp.cpu_count()))
+        
+        #Reducer Job
         reducer_in = sorter2(combiner_out)
-        print(reducer(reducer_in))
+        reducer_out = reducer(reducer_in)
+        
+        # Displaying output in tabular form
+        headings = ["Passenger id", "Maximum Flights"]
+        Table = pd.DataFrame(columns = headings, data = reducer_out)
+        print('"Passengers having maximum number of flights"\n\n',Table)
+        
+        # Output to csv file
+        Table.to_csv('Task2_Results.csv', index = False, header = True)
+        
+
+############################################################
+
+""" ENTERING MAIN FILE FOR EXECUTION """
+
+if __name__ == "__main__":
+    main()
+   
+##############################################################   
